3fe17e995802a723bdaa8b9a4b9505d2eace5462
David Keller <email": null>
2017-08-03 15:51:38 -0700

    Add all built in methods

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -9,18 +9,28 @@ end
 
 #puts encrypt("abc")
 
+# def decrypt(string)
+# alphabete = "abcdefghijklmnopqrstuvwxyz"
+# new_string = ''
+# index = 0
+#   while index < string.length
+#     new_letter = alphabete[alphabete.index(string[index])-1]
+#     new_string += new_letter
+#     index += 1
+#   end
+# new_string
+# end
+
 def decrypt(string)
-alphabete = "abcdefghijklmnopqrstuvwxyz"
-new_string = ''
 index = 0
   while index < string.length
-    new_letter = alphabete[alphabete.index(string[index])-1]
-    new_string += new_letter
-    index += 1
+    #new_string = string.reverse
+    string[index] = (string[index] -1)
+  index += 1
   end
-new_string
+string
 end
 
-#puts decrypt("abc")
+puts decrypt("david")
 
 #puts decrypt(encrypt("swordfish"))

09451bcc460e4a1b13ceee550ae196d2e5734908
David Keller <email": null>
2017-08-04 15:49:57 -0700

    Add methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,35 @@
 class Puppy
 
+  def initialize
+    #puts "Initializing new puppy instance"
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(num)
+    puts "Woof! " * num
+  end
+
+  def roll_over
+    puts "rolls over*"
+  end
+
+  def dog_years(num)
+    dog_year = num.to_i * 8
+    puts "Puppy is #{dog_year} years old"
+  end
+
 end
 
+50.times do
+  array = []
+  array << Puppy.new
+  array.each { |instance| instance.fetch("ball") }
+end
+# puppy1.fetch("ball")
+# puppy1.speak(2)
+# puppy1.roll_over
+# puppy1.dog_years(7)
